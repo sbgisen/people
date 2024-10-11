8d5641d8ae0583b554253709a7f37e182d397dd1
Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
2024-10-11 16:57:32 +0900

    Add launch

---
+++ leg_detector/launch/leg_detector.launch.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2024 SoftBank Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description() -> LaunchDescription:
+    """Generate launch descriptions.
+
+    Returns:
+        Launch descriptions
+    """
+    return LaunchDescription([
+        Node(package='leg_detector',
+             executable='leg_detector',
+             name='leg_detector',
+             arguments=[
+                 [FindPackageShare('leg_detector'), '/config/trained_leg_detector.yaml']
+             ],
+             remappings=[('scan', 'base_scan')],
+             output='screen')
+    ])
