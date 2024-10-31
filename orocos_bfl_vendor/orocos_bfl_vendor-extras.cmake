f57186f64f27326b573e4bebcfaea161c3b61229
Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
2024-10-31 10:25:00 +0900

    Fix path

--- orocos_bfl_vendor/orocos_bfl_vendor-extras.cmake
+++ orocos_bfl_vendor/orocos_bfl_vendor-extras.cmake
@@ -1,7 +1,6 @@
 
 find_package(PkgConfig)
-get_filename_component(INSTALL_DIR "${CMAKE_INSTALL_PREFIX}" DIRECTORY)
-set(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:${INSTALL_DIR}/orocos_bfl_vendor/opt/orocos_bfl_vendor/lib/pkgconfig")
+set(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:${orocos_bfl_vendor_DIR}/../../../opt/orocos_bfl_vendor/lib/pkgconfig")
 
 pkg_check_modules(
   BFL
